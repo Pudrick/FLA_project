dc8b6dbbb54153052a47cc0fe0e870c7e7d4015a
Pudrick <Pudrick@outlook.com>
2021-12-12 00:01:50 +0800

    removed some cout

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -12,10 +12,10 @@ analyzer::analyzer(string tm_name)
 {
     tm = tm_name;
     tm_file.open(tm_name);
-    std::cout << "opening begins and tm_name is " << tm_name << std::endl;
+    // std::cout << "opening begins and tm_name is " << tm_name << std::endl;
     if(tm_file.is_open() == false)
     {
-        std::cout << "Can't open TM file." << std::endl;
+        std::cerr << "Can't open TM file." << std::endl;
         std::exit(1);
     }
     std::cout << "if has ended" << std::endl;
