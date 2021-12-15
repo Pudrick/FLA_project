606f8d053309d44c0fcc435f7079b48e8457bb84
Pudrick <Pudrick@outlook.com>
2021-12-15 23:22:57 +0800

    Add more symbols TODO: rebuild by regex

--- include/analyzer.h
+++ include/analyzer.h
@@ -9,27 +9,27 @@ using std::string;
 class analyzer
 {
     public:
-    analyzer(string tm_name, bool v_mode);
-    void set_verbose(bool value);
+        analyzer(string tm_name, bool v_mode);
+        void set_verbose(bool value);
 
     private:
-    string tm;
-    std::ifstream tm_file;
-    std::vector<string> Q;
-    std::vector<string> S;
-    std::vector<string> G;
-    string q0;
-    const char blank = '_';
-    std::vector<string> F;
-    int N;
-    struct trans_func{
-        string prev_state;
-        string new_state;
-        string prev_symbols;
-        string new_symbols;
-        string directions;
-    }delta;
-    void tmFileAna(std::ifstream &filestream);// Analyze the imput
-    // std::ifstream as arguments need to be &.
-    bool VERBOSE_MODE = false;
+        string tm;
+        std::ifstream tm_file;
+        std::vector<string> Q;
+        std::vector<char> S;
+        std::vector<char> G;
+        string q0;
+        char B = '_';
+        std::vector<string> F;
+        int N;
+        struct trans_func{
+            string prev_state;
+            string new_state;
+            string prev_symbols;
+            string new_symbols;
+            string directions;
+        }delta;
+        void tmFileAna(std::ifstream &filestream);// Analyze the imput
+        // std::ifstream as arguments need to be &.
+        bool VERBOSE_MODE = false;
 };
--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -4,10 +4,13 @@
 #include<vector>
 #include<fstream>
 #include<limits>
+#include<algorithm>
 
 #include"../include/analyzer.h"
 
 using std::string;
+
+#define IGNORE_WHOLE_LINE std::numeric_limits<std::streamsize>::max()
 
 void analyzer::set_verbose(bool value)
 {
@@ -45,7 +48,8 @@ void analyzer::tmFileAna(std::ifstream &filestream)
         if(first == ';' || first == 10 || first == 0 || first == -1)// ASCII 10 is '\n'.
         {
             // why the last empty line will get first == -1 ??
-            std::cout << "get a comment" << std::endl;
+            // maybe -1 represent to eof.
+            std::cout << "get a comment with first " << int(first) << std::endl;
             filestream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             continue;
         }
@@ -57,8 +61,89 @@ void analyzer::tmFileAna(std::ifstream &filestream)
             string flag = flag_temp;
             if(flag == "#Q")
             {
+                filestream.ignore(std::numeric_limits<std::streamsize>::max(),'{');
+                string state_set;
+                std::getline(filestream,state_set,'}');
+                std::cout << "state set is " << state_set << std::endl;
+                auto pos = 0;
+                while((pos = state_set.find(','))  !=  string::npos)
+                {
+                    Q.push_back(state_set.substr(0, pos));
+                    std::cout << "push is " << state_set.substr(0, pos) << std::endl;
+                    state_set.erase(0,pos + 1);// 1 is length for ','
+                }// the last token don't have ',' so have to push it manually.
+                Q.push_back(state_set);
                 
+                //just for more robustness.
+                for(auto str : Q)
+                    str.erase(std::remove(str.begin(),str.end(),' '),str.end());// WTF?? MAGIC!
+                    // std::cout << "token is " << i << std::endl;
             }
+            if(flag == "#S")
+            {
+                std::vector<char> S_ban_list = {' ', ',', ';', '{', '}', '*', '_'};
+                filestream.ignore(std::numeric_limits<std::streamsize>::max(),'{');
+                string input_set;
+                std::getline(filestream, input_set, '}');
+                std::cout << "input set is " << input_set << std::endl;
+                for(auto ch : input_set)
+                {
+                    if(ch == ',')
+                        continue;
+                    for(auto check : S_ban_list)
+                        if(ch == check)
+                        {
+                            std::cerr << "Invalid input symbol." << std::endl;
+                            std::exit(3);
+                        }
+                    std::cout << "to append in S is " << ch << std::endl;
+                    S.push_back(ch);
+                }
+            }
+            if(flag == "#G")
+            {
+                std::vector<char> G_ban_list = {' ', ',', ';', '{', '}', '*'};
+                filestream.ignore(std::numeric_limits<std::streamsize>::max(),'{');
+                string tape_set;
+                std::getline(filestream, tape_set, '}');
+                std::cout << "tape set is " << tape_set << std::endl;
+                for(auto ch : tape_set)
+                {
+                    if(ch == ',')
+                        continue;
+                    for(auto check : G_ban_list)
+                        if(ch == check)
+                        {
+                            std::cerr << "Invalid input symbol." << std::endl;
+                            std::exit(3);
+                        }
+                    std::cout << "to append in G is " << ch << std::endl;
+                    G.push_back(ch);
+                }
+            }
+            if(flag == "#q")
+            {
+                char q_flag;
+                filestream.get(&q_flag,2);
+                std::cout << "qflag is " << q_flag << std::endl;
+                if(q_flag != '0')
+                {
+                    std::cerr << "invalid flag of q0" << std::endl;
+                    std::exit(4);
+                }
+                filestream.ignore(IGNORE_WHOLE_LINE, '=');
+                filestream.ignore();// there is a ' ' after '='
+                std::getline(filestream, q0);
+                std::cout << "q0 is " << q0 << std::endl;
+            }
+            if(flag == "#B")
+            {
+                filestream.ignore(IGNORE_WHOLE_LINE,'=');
+                filestream.ignore();
+                B = filestream.get();
+                std::cout << "B is " << B << std::endl;
+            }
+            
         }
     }
 }
